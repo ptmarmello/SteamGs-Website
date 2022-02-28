91d4479b779490130b89be84ae820ab4cfdee60a
Pedro Thiago <ptmarmello@gmail.com>
2022-02-27 23:34:57 -0300

    fixing companies page

--- PEI-Version/frontend/pages/companies.js
+++ PEI-Version/frontend/pages/companies.js
@@ -3,11 +3,9 @@ import styles from '../styles/Home.module.css';
 import Advantages from '../files/components/Advantages';
 import Forms from '../files/components/Forms/Forms';
 import Insightsection from '../files/components/InsightSection';
-// import MainSection from '../files/components/MainSection/MainSection';
 import Whereabouts from '../files/components/Whereabouts';
 import Workin from '../files/components/Workin';
 import webconfig from './data/webConfig.json';
-// import Link from 'next/link';
 
 export default function Companies() {
   let WAConfig = webconfig.components.companies.whereabouts;
