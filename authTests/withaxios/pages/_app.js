7b70318b84edccbf55799ad54689ea5bfaa2f65c
Pethi <ptmarmello@gmail.com>
2022-08-19 02:25:45 +0000

    Login With Axios test

---
+++ authTests/withaxios/pages/_app.js
@@ -0,0 +1,14 @@
+import { AuthProvider, ProtectRoute } from '../contexts/auth';
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return(
+    <AuthProvider>
+      <ProtectRoute>
+        <Component {...pageProps} />;
+      </ProtectRoute>
+    </AuthProvider>
+  )
+}
+
+export default MyApp
