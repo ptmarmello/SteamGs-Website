ad4e6fcdde53520ebf6c7148553ae1e2ff455480
unknown <ptmarmello@gmail.com>
2022-11-12 23:14:03 -0300

    landing page new version of first section

--- authTests/withSupabase/next.config.js
+++ authTests/withSupabase/next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const withVideos = require('next-videos')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  withVideos: withVideos,
+  output: {
+    filename: 'my-first-webpack.bundle.js',
+  },
 }
 
 module.exports = nextConfig
+module.exports = withVideos({})
